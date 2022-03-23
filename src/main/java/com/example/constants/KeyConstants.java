ed06b0a7715d2e7f57393d6e832672cbf82ecdd0
Deepak Katariya <deepak.sorthiya@gmail.com>
2022-03-23 12:55:56 +0530

    added contans

--- src/main/java/com/example/constants/KeyConstants.java
+++ src/main/java/com/example/constants/KeyConstants.java
@@ -2,7 +2,7 @@ package com.example.constants;
 
 public class KeyConstants {
 
-	public static final String API_PREFIX = "/api";
+	public static final String API_PREFIX = "/api/v1";
 
 	private KeyConstants() {
 	}
